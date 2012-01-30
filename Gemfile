5779b42933ae33cdd8e854a83f4c5f4db0368987
George Guimarães <george@plataformatec.com.br>
2012-01-30 18:59:06 -0200

    list hooks for the given github repo

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in github-hooker.gemspec
 gemspec
+
+gem 'rspec'
+gem 'webmock'
--- lib/github-hooker.rb
+++ lib/github-hooker.rb
@@ -1,6 +1,55 @@
 require "github-hooker/version"
+require 'net/http'
+require 'json'
+require 'yaml'
+require 'restclient'
 
 module Github
   module Hooker
+
+    def self.add_hook(options={})
+      payload = {
+        :name => options[:name],
+        :events => options[:events],
+        :active => true,
+      }
+
+      request_options = {
+        :method => :post,
+        :url => "https://api.github.com/repos/#{options[:repo]}/hooks",
+        :user => config[:user],
+        :password => config[:password],
+        :payload => payload
+      }
+      response = RestClient::Request.execute(request_options)
+      response = JSON.parse(response)
+    end
+
+    def self.list_hooks(options={})
+      request_options = {
+        :method => :get,
+        :url => "https://api.github.com/repos/#{options[:repo]}/hooks",
+        :user => config["user"],
+        :password => config["password"],
+        :ssl => true
+      }
+      response = RestClient::Request.execute(request_options)
+      response = JSON.parse(response)
+
+      response.each do |hook|
+        puts "#{hook['url']}"
+        puts "> name:   #{hook['name']}"
+        puts "> events: #{hook['events'].join(", ")}"
+        puts
+      end
+    end
+
+    def self.config
+      @config ||= YAML.load_file(File.expand_path("~/.github-hooker.yml"))
+    end
+
+    def self.github_api
+
+    end
   end
 end
